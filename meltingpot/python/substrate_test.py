google-deepmind/meltingpot

commit 449ac8b083c5bbb0d338404e7094d63c00beb5e3

Fix setting and usage of env_seed in meltingpot builder.
PiperOrigin-RevId: 389247840
Change-Id: Ie4ec55ef62b858373f617f5d4252f141f9f67447

meltingpot/python/utils/substrates/builder.py
@@ -14,6 +14,7 @@
 """Multi-player environment builder for Melting Pot levels."""
 
 import copy
+import itertools
 import os
 import pathlib  # pylint: disable=unused-import
 import random
@@ -133,14 +134,6 @@ def locate_and_overwrite_level_directory(
     lab2d_settings.levelDirectory = _MELTINGPOT_ROOT
 
 
-def set_env_seed(lab2d_settings: config_dict.ConfigDict,
-                 env_seed: Optional[int] = None,):
-  if env_seed is None:
-    # Select a long seed different than zero.
-    env_seed = random.randint(1, _MAX_SEED)
-  lab2d_settings.env_seed = env_seed
-
-
 def builder(
     lab2d_settings: Settings,
     prefab_overrides: Optional[Settings] = None,
@@ -162,6 +155,10 @@ def builder(
 
   assert "simulation" in lab2d_settings
 
+  if env_seed is None:
+    # Select a long seed different than zero.
+    env_seed = random.randint(1, _MAX_SEED)
+
   # Copy config, so as not to modify it.
   lab2d_settings = config_dict.ConfigDict(
       copy.deepcopy(lab2d_settings)).unlock()
@@ -169,7 +166,6 @@ def builder(
   apply_prefab_overrides(lab2d_settings, prefab_overrides)
   maybe_build_and_add_avatar_objects(lab2d_settings)
   locate_and_overwrite_level_directory(lab2d_settings)
-  set_env_seed(lab2d_settings, env_seed)
 
   # Convert settings from python to Lua format.
   lab2d_settings_dict = parse_python_settings_for_dmlab2d(lab2d_settings)
@@ -180,16 +176,17 @@ def builder(
   logging.info("available observation names: %s", observation_names)
 
   # Wrap the raw environment with the dm_env API.
-  env = dmlab2d.Environment(env_raw, observation_names)
+  env = dmlab2d.Environment(env_raw, observation_names, env_seed)
+
+  seeds_iter = itertools.count(env_seed)
 
   def rebuild_environment():
-    seed = random.randint(1, _MAX_SEED)
-    lab2d_settings_dict["env_seed"] = str(seed)
+    seed = next(seeds_iter) % (_MAX_SEED + 1)
     env_raw = dmlab2d.Lab2d(runfiles_helper.find(), lab2d_settings_dict)
     return dmlab2d.Environment(
         env=env_raw,
         observation_names=observation_names,
-        seed=random.randint(1, _MAX_SEED))
+        seed=seed)
 
   # Add a wrapper that rebuilds the environment when reset is called.
   env = reset_wrapper.ResetWrapper(env, rebuild_environment)

meltingpot/python/substrate_test.py
@@ -27,6 +27,22 @@ ACTION_SPEC = dm_env.specs.DiscreteArray(
 
 class SubstrateTest(parameterized.TestCase):
 
+  @parameterized.parameters(
+      [[42], [123], [1337], [12481632]])
+  def test_seed_causes_determinism(self, seed):
+    config = substrate.get_config('allelopathic_harvest')
+    with config.unlocked():
+      config.env_seed = seed
+
+    env = substrate.build(config)
+    prev_obs = env.reset().observation[0]
+    obs = []
+    for _ in range(10):
+      env = substrate.build(config)
+      obs.append(env.reset().observation[0])
+
+    np.testing.assert_equal(obs, [prev_obs] * 10)
+
   @parameterized.named_parameters(
       (name, name) for name in substrate.AVAILABLE_SUBSTRATES)
   def test_substrate_creation(self, substrate_name):

commit c009d0a601670c51107994e4bf63db6ed2f71c0a

BUGFIX: Ensure Python seed is forwarded to Lua.
https://github.com/deepmind/meltingpot/commit/449ac8b083c5bbb0d338404e7094d63c00beb5e3 introduced a bug where the random seed was no longer forwarded from Python to Lua, meaning that the Lua-controlled parts of the environment were not randomized. This change fixes this and adds tests of Lua randomization.

NOTE: This did not affect the experiments in the Melting Pot paper as the bug was introduced after these experiments were run.
PiperOrigin-RevId: 400239767
Change-Id: I905e4c7ec7ca0324dbf6df1f94dd048953bc8aad

meltingpot/python/utils/substrates/builder.py
@@ -171,6 +171,7 @@ def builder(
   lab2d_settings_dict = parse_python_settings_for_dmlab2d(lab2d_settings)
 
   # Only the raw environment has the properties API.
+  lab2d_settings_dict["env_seed"] = str(env_seed)  # Sets the Lua seed.
   env_raw = dmlab2d.Lab2d(_DMLAB2D_ROOT, lab2d_settings_dict)
   observation_names = env_raw.observation_names()
   logging.info("available observation names: %s", observation_names)
@@ -182,6 +183,7 @@ def builder(
 
   def rebuild_environment():
     seed = next(seeds_iter) % (_MAX_SEED + 1)
+    lab2d_settings_dict["env_seed"] = str(seed)  # Sets the Lua seed.
     env_raw = dmlab2d.Lab2d(runfiles_helper.find(), lab2d_settings_dict)
     return dmlab2d.Environment(
         env=env_raw,

meltingpot/python/substrate_test.py
@@ -25,6 +25,19 @@ ACTION_SPEC = dm_env.specs.DiscreteArray(
     num_values=1, dtype=np.int32, name='action')
 
 
+def _get_lua_randomization_map():
+  """Replaces first row of walls with items randomized by Lua."""
+  config = substrate.get_config('running_with_scissors_in_the_matrix')
+  head, line, *tail = config.lab2d_settings.simulation.map.split('\n')
+  # Replace line 1 (walls) with a row of 'a' (items randomized by Lua).
+  new_map = '\n'.join([head, 'a' * len(line), *tail])
+  return new_map
+
+
+_LUA_RANDOMIZED_LINE = 1
+_LUA_RANDOMIZATION_MAP = _get_lua_randomization_map()
+
+
 class SubstrateTest(parameterized.TestCase):
 
   @parameterized.product(seed=[42, 123, 1337, 12481632])
@@ -69,6 +82,38 @@ class SubstrateTest(parameterized.TestCase):
       obs2 = env2.reset().observation[0]['WORLD.RGB']
       with self.assertRaises(
           AssertionError, msg=f'Episode {episode} match: {obs1} == {obs2}'):
+        np.testing.assert_equal(obs1, obs2)
+
+  @parameterized.product(seed=[None, 42, 123, 1337, 12481632])
+  def test_episodes_are_randomized_in_lua(self, seed):
+    config = substrate.get_config('running_with_scissors_in_the_matrix')
+    with config.unlocked():
+      config.env_seed = seed
+      config.lab2d_settings.simulation.map = _LUA_RANDOMIZATION_MAP
+    env = substrate.build(config)
+
+    obs = env.reset().observation[0]['WORLD.RGB'][_LUA_RANDOMIZED_LINE]
+    for episode in range(4):
+      last_obs = obs
+      obs = env.reset().observation[0]['WORLD.RGB'][_LUA_RANDOMIZED_LINE]
+      with self.assertRaises(
+          AssertionError,
+          msg=f'Episodes {episode} and {episode+1} match: {last_obs} == {obs}'):
+        np.testing.assert_equal(last_obs, obs)
+
+  def test_no_seed_causes_nondeterminism_for_lua(self):
+    config = substrate.get_config('running_with_scissors_in_the_matrix')
+    with config.unlocked():
+      config.env_seed = None
+      config.lab2d_settings.simulation.map = _LUA_RANDOMIZATION_MAP
+
+    env1 = substrate.build(config)
+    env2 = substrate.build(config)
+    for episode in range(5):
+      obs1 = env1.reset().observation[0]['WORLD.RGB'][_LUA_RANDOMIZED_LINE]
+      obs2 = env2.reset().observation[0]['WORLD.RGB'][_LUA_RANDOMIZED_LINE]
+      with self.assertRaises(
+          AssertionError, msg=f'Episode {episode} match {obs1} == {obs2}'):
         np.testing.assert_equal(obs1, obs2)
 
   @parameterized.named_parameters(