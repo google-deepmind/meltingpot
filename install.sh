3eaf9aaa5e3d7d56664184ce36be31ad5c0f9c39
John Agapiou <jagapiou@users.noreply.github.com>
2022-03-16 12:01:36 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,30 +17,29 @@
 
 set -e  # Fail on any error.
 set -u  # Fail on unset variables.
+set -x  # Echo all comands.
 
 readonly MELTINGPOT_ROOT="$(dirname "$(realpath $0)")"
 
 # Set this to where you would like create the virtual env.
 readonly VENV_PATH="${MELTINGPOT_ROOT}/venv"
 
-# Build dmlab2d wheel from src if desired.
-readonly BUILD_DMLAB2D=0
-if (( ${BUILD_DMLAB2D} )); then
-  echo "Building dmlab2d wheel..."
-  sudo apt-get update
-  sudo apt-get install bazel
-  cd "${MELTINGPOT_ROOT}"
-  git clone https://github.com/deepmind/lab2d
-  cd lab2d
-  bazel build -c opt --config=lua5_2 //dmlab2d:dmlab2d_wheel
-  readonly DMLAB_WHEEL_ROOT="${MELTINGPOT_ROOT}/lab2d/bazel-bin/dmlab2d"
-else
-  readonly DMLAB_WHEEL_ROOT='https://github.com/deepmind/lab2d/releases/download/release_candidate_2021-07-13'
-fi
-
 echo "Creating venv..."
 python3 -m venv "${VENV_PATH}"
 source "${VENV_PATH}/bin/activate"
+python --version
+
+echo "Installing build dependencies..."
+pip install --upgrade pip setuptools build
+
+echo "Building dmlab2d wheel..."
+sudo apt-get update
+sudo apt-get install bazel
+cd "${MELTINGPOT_ROOT}"
+git clone https://github.com/deepmind/lab2d
+cd lab2d
+bazel build -c opt --config=lua5_2 //dmlab2d:dmlab2d_wheel
+readonly DMLAB_WHEEL_ROOT="${MELTINGPOT_ROOT}/lab2d/bazel-bin/dmlab2d"
 
 echo "Installing dmlab2d..."
 readonly VERSION="$(python --version | sed 's/^Python \([0-9]\)\.\([0-9]\+\)\..*$/\1\2/g')"
