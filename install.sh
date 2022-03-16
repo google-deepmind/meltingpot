e3d4a113a8df92d6e87340edb922c763dd665775
John Agapiou <jagapiou@users.noreply.github.com>
2022-03-16 13:54:41 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,48 +19,57 @@ set -euxo pipefail
 
 readonly MELTINGPOT_ROOT="$(dirname "$(realpath $0)")"
 
-# Set this to where you would like create the virtual env.
-readonly VENV_PATH="${MELTINGPOT_ROOT}/venv"
 
-echo "Creating venv..."
-python3 -m venv "${VENV_PATH}"
-source "${VENV_PATH}/bin/activate"
-python --version
+function setup_venv() {
+  echo
+  echo "Creating venv..."
+  python3 -m venv meltingpot_testing
+  source meltingpot_testing/bin/activate
+  python --version
+}
 
-echo "Installing build dependencies..."
-pip install --upgrade pip setuptools build
 
-echo "Installing bazel..."
-apt install apt-transport-https curl gnupg
-curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
-mv bazel.gpg /etc/apt/trusted.gpg.d/
-echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
-apt-get update && apt-get install bazel
+function install_bazel() {
+  echo
+  echo "Installing bazel..."
+  apt install apt-transport-https curl gnupg
+  curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > "${TMP}/bazel.gpg"
+  mv "${TMP}/bazel.gpg" /etc/apt/trusted.gpg.d/
+  echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
+  apt-get update && apt-get install bazel
+}
 
-echo "Building dmlab2d wheel..."
-cd "${MELTINGPOT_ROOT}"
-git clone https://github.com/deepmind/lab2d
-cd lab2d
-if [[ "$(uname -s)" == 'Linux' ]]; then
-  readonly LUA_VERSION=luajit
-else
-  readonly LUA_VERSION=lua5_2
-fi
-bazel build \
-    --compilation_mode=opt \
-    --config="${LUA_VERSION}" \
-    --verbose_failures \
-    --experimental_ui_max_stdouterr_bytes=-1 \
-    --toolchain_resolution_debug \
-    --sandbox_debug \
-    //dmlab2d:dmlab2d_wheel
 
-echo "Installing dmlab2d..."
-pip install "${MELTINGPOT_ROOT}"/lab2d/bazel-bin/dmlab2d/dmlab2d-*.whl
+function install_dmlab2d() {
+  echo
+  echo "Building dmlab2d wheel..."
+  git clone https://github.com/deepmind/lab2d
+  if [[ "$(uname -s)" == 'Linux' ]]; then
+    readonly LUA_VERSION=luajit
+  else
+    readonly LUA_VERSION=lua5_2
+  fi
+  pushd lab2d
+  bazel build \
+      --compilation_mode=opt \
+      --config="${LUA_VERSION}" \
+      --verbose_failures \
+      --experimental_ui_max_stdouterr_bytes=-1 \
+      --toolchain_resolution_debug \
+      --sandbox_debug \
+      //dmlab2d:dmlab2d_wheel
+  popd
 
-echo "Testing dmlab2d..."
-cd "${HOME}"
-python - <<'____HERE'
+  echo
+  echo "Installing dmlab2d..."
+  pip install lab2d/bazel-bin/dmlab2d/dmlab2d-*.whl
+}
+
+
+function test_dmlab2d() {
+  echo
+  echo "Testing dmlab2d..."
+  python - <<'____HERE'
 import dmlab2d
 import dmlab2d.runfiles_helper
 
@@ -68,11 +77,33 @@ lab = dmlab2d.Lab2d(dmlab2d.runfiles_helper.find(), {"levelName": "chase_eat"})
 env = dmlab2d.Environment(lab, ["WORLD.RGB"])
 env.step({})
 ____HERE
+}
 
-echo "Installing meltingpot..."
-cd "${MELTINGPOT_ROOT}"
-pip install .
 
-echo "Testing meltingpot..."
-pip install pytest-xdist
-pytest -n auto -ra --durations=10 meltingpot
+function install_meltingpot() {
+  echo
+  echo "Installing meltingpot..."
+  cd "${MELTINGPOT_ROOT}"
+  pip install .
+}
+
+
+function test_meltingpot() {
+  echo
+  echo "Testing meltingpot..."
+  pip install pytest-xdist
+  pytest -n auto -ra --durations=10 meltingpot
+}
+
+
+function main() {
+  install_bazel
+  setup_venv
+  install_dmlab2d
+  test_dmlab2d
+  install_meltingpot
+  test_meltingpot
+}
+
+
+main "$@"
