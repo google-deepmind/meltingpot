e9d1ae2d84458ee34d29f5dd1166eab6d820f968
John Agapiou <jagapiou@google.com>
2022-11-04 06:49:37 -0700

    Enter context when building substrates and scenarios to ensure cleanup
    PiperOrigin-RevId: 486129117
    Change-Id: I3468a809857b2e6dd384c0be0db6a3d1c1ee6935

--- meltingpot/python/substrate_test.py
+++ meltingpot/python/substrate_test.py
@@ -20,10 +20,12 @@ import numpy as np
 from meltingpot.python import substrate
 from meltingpot.python.testing import substrates as test_utils
 
+_TEST_SUBSTRATE = 'running_with_scissors_in_the_matrix'
+
 
 def _get_lua_randomization_map():
   """Replaces first row of walls with items randomized by Lua."""
-  config = substrate.get_config('running_with_scissors_in_the_matrix')
+  config = substrate.get_config(_TEST_SUBSTRATE)
   head, line, *tail = config.lab2d_settings.simulation.map.split('\n')
   # Replace line 1 (walls) with a row of 'a' (items randomized by Lua).
   new_map = '\n'.join([head, 'a' * len(line), *tail])
@@ -38,12 +40,12 @@ class GeneralTestCase(parameterized.TestCase):
 
   @parameterized.product(seed=[42, 123, 1337, 12481632])
   def test_seed_causes_determinism(self, seed):
-    config = substrate.get_config('running_with_scissors_in_the_matrix')
+    config = substrate.get_config(_TEST_SUBSTRATE)
     with config.unlocked():
       config.env_seed = seed
 
-    env1 = substrate.build(config)
-    env2 = substrate.build(config)
+    env1 = self.enter_context(substrate.build(config))
+    env2 = self.enter_context(substrate.build(config))
     for episode in range(5):
       obs1 = env1.reset().observation[0]['WORLD.RGB']
       obs2 = env2.reset().observation[0]['WORLD.RGB']
@@ -52,10 +54,10 @@ class GeneralTestCase(parameterized.TestCase):
 
   @parameterized.product(seed=[None, 42, 123, 1337, 12481632])
   def test_episodes_are_randomized(self, seed):
-    config = substrate.get_config('running_with_scissors_in_the_matrix')
+    config = substrate.get_config(_TEST_SUBSTRATE)
     with config.unlocked():
       config.env_seed = seed
-    env = substrate.build(config)
+    env = self.enter_context(substrate.build(config))
 
     obs = env.reset().observation[0]['WORLD.RGB']
     for episode in range(4):
@@ -67,12 +69,12 @@ class GeneralTestCase(parameterized.TestCase):
         np.testing.assert_equal(last_obs, obs)
 
   def test_no_seed_causes_nondeterminism(self):
-    config = substrate.get_config('running_with_scissors_in_the_matrix')
+    config = substrate.get_config(_TEST_SUBSTRATE)
     with config.unlocked():
       config.env_seed = None
 
-    env1 = substrate.build(config)
-    env2 = substrate.build(config)
+    env1 = self.enter_context(substrate.build(config))
+    env2 = self.enter_context(substrate.build(config))
     for episode in range(5):
       obs1 = env1.reset().observation[0]['WORLD.RGB']
       obs2 = env2.reset().observation[0]['WORLD.RGB']
@@ -82,11 +84,11 @@ class GeneralTestCase(parameterized.TestCase):
 
   @parameterized.product(seed=[None, 42, 123, 1337, 12481632])
   def test_episodes_are_randomized_in_lua(self, seed):
-    config = substrate.get_config('running_with_scissors_in_the_matrix')
+    config = substrate.get_config(_TEST_SUBSTRATE)
     with config.unlocked():
       config.env_seed = seed
       config.lab2d_settings.simulation.map = _LUA_RANDOMIZATION_MAP
-    env = substrate.build(config)
+    env = self.enter_context(substrate.build(config))
 
     obs = env.reset().observation[0]['WORLD.RGB'][_LUA_RANDOMIZED_LINE]
     for episode in range(4):
@@ -98,13 +100,13 @@ class GeneralTestCase(parameterized.TestCase):
         np.testing.assert_equal(last_obs, obs)
 
   def test_no_seed_causes_nondeterminism_for_lua(self):
-    config = substrate.get_config('running_with_scissors_in_the_matrix')
+    config = substrate.get_config(_TEST_SUBSTRATE)
     with config.unlocked():
       config.env_seed = None
       config.lab2d_settings.simulation.map = _LUA_RANDOMIZATION_MAP
 
-    env1 = substrate.build(config)
-    env2 = substrate.build(config)
+    env1 = self.enter_context(substrate.build(config))
+    env2 = self.enter_context(substrate.build(config))
     for episode in range(5):
       obs1 = env1.reset().observation[0]['WORLD.RGB'][_LUA_RANDOMIZED_LINE]
       obs2 = env2.reset().observation[0]['WORLD.RGB'][_LUA_RANDOMIZED_LINE]
@@ -119,13 +121,13 @@ class SubstrateTestCase(test_utils.SubstrateTestCase):
 
   def test_matches_spec(self, name):
     config = substrate.get_config(name)
-    with substrate.build(config) as env:
-      with self.subTest('discount'):
-        self.assert_discount_matches_spec(env)
-      with self.subTest('reward'):
-        self.assert_reward_matches_spec(env)
-      with self.subTest('observation'):
-        self.assert_observation_matches_spec(env)
+    env = self.enter_context(substrate.build(config))
+    with self.subTest('discount'):
+      self.assert_discount_matches_spec(env)
+    with self.subTest('reward'):
+      self.assert_reward_matches_spec(env)
+    with self.subTest('observation'):
+      self.assert_observation_matches_spec(env)
 
   def test_spec_in_config_matches_environment(self, name):
     config = substrate.get_config(name)
