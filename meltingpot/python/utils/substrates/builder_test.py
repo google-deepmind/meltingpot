995be246fb3e37d94f0611c12f19e4c83f8c2ef8
John Agapiou <jagapiou@google.com>
2022-11-15 16:29:00 -0800

    Update builder test to directly use specific config's lab2d_settings.
    PiperOrigin-RevId: 488789125
    Change-Id: I211e8a7311e190595de35c28d2f4f80eec402504

--- meltingpot/python/utils/substrates/builder_test.py
+++ meltingpot/python/utils/substrates/builder_test.py
@@ -19,16 +19,16 @@ from absl.testing import absltest
 from absl.testing import parameterized
 import numpy as np
 
-from meltingpot.python.configs import substrates as substrate_configs
+from meltingpot.python.configs.substrates import running_with_scissors_in_the_matrix
 from meltingpot.python.utils.substrates import builder
 
-_TEST_CONFIG = substrate_configs.get_config(
-    'running_with_scissors_in_the_matrix')
+
+_TEST_SETTINGS = running_with_scissors_in_the_matrix.get_config().lab2d_settings
 
 
 def _get_lua_randomization_map():
   """Replaces first row of walls with items randomized by Lua."""
-  head, line, *tail = _TEST_CONFIG.lab2d_settings.simulation.map.split('\n')
+  head, line, *tail = _TEST_SETTINGS.simulation.map.split('\n')
   # Replace line 1 (walls) with a row of 'a' (items randomized by Lua).
   new_map = '\n'.join([head, 'a' * len(line), *tail])
   return new_map
@@ -42,11 +42,8 @@ class GeneralTestCase(parameterized.TestCase):
 
   @parameterized.product(seed=[42, 123, 1337, 12481632])
   def test_seed_causes_determinism(self, seed):
-    config = copy.deepcopy(_TEST_CONFIG).unlock()
-    config.env_seed = seed
-
-    env1 = self.enter_context(builder.builder(**config))
-    env2 = self.enter_context(builder.builder(**config))
+    env1 = self.enter_context(builder.builder(_TEST_SETTINGS, env_seed=seed))
+    env2 = self.enter_context(builder.builder(_TEST_SETTINGS, env_seed=seed))
     for episode in range(5):
       obs1 = env1.reset().observation['WORLD.RGB']
       obs2 = env2.reset().observation['WORLD.RGB']
@@ -55,9 +52,7 @@ class GeneralTestCase(parameterized.TestCase):
 
   @parameterized.product(seed=[None, 42, 123, 1337, 12481632])
   def test_episodes_are_randomized(self, seed):
-    config = copy.deepcopy(_TEST_CONFIG).unlock()
-    config.env_seed = seed
-    env = self.enter_context(builder.builder(**config))
+    env = self.enter_context(builder.builder(_TEST_SETTINGS, env_seed=seed))
 
     obs = env.reset().observation['WORLD.RGB']
     for episode in range(4):
@@ -69,11 +64,8 @@ class GeneralTestCase(parameterized.TestCase):
         np.testing.assert_equal(last_obs, obs)
 
   def test_no_seed_causes_nondeterminism(self):
-    config = copy.deepcopy(_TEST_CONFIG).unlock()
-    config.env_seed = None
-
-    env1 = self.enter_context(builder.builder(**config))
-    env2 = self.enter_context(builder.builder(**config))
+    env1 = self.enter_context(builder.builder(_TEST_SETTINGS, env_seed=None))
+    env2 = self.enter_context(builder.builder(_TEST_SETTINGS, env_seed=None))
     for episode in range(5):
       obs1 = env1.reset().observation['WORLD.RGB']
       obs2 = env2.reset().observation['WORLD.RGB']
@@ -83,10 +75,9 @@ class GeneralTestCase(parameterized.TestCase):
 
   @parameterized.product(seed=[None, 42, 123, 1337, 12481632])
   def test_episodes_are_randomized_in_lua(self, seed):
-    config = copy.deepcopy(_TEST_CONFIG).unlock()
-    config.env_seed = seed
-    config.lab2d_settings.simulation.map = _LUA_RANDOMIZATION_MAP
-    env = self.enter_context(builder.builder(**config))
+    lab2d_settings = copy.deepcopy(_TEST_SETTINGS)
+    lab2d_settings.simulation.map = _LUA_RANDOMIZATION_MAP
+    env = self.enter_context(builder.builder(lab2d_settings, env_seed=seed))
 
     obs = env.reset().observation['WORLD.RGB'][_LUA_RANDOMIZED_LINE]
     for episode in range(4):
@@ -98,12 +89,11 @@ class GeneralTestCase(parameterized.TestCase):
         np.testing.assert_equal(last_obs, obs)
 
   def test_no_seed_causes_nondeterminism_for_lua(self):
-    config = copy.deepcopy(_TEST_CONFIG).unlock()
-    config.env_seed = None
-    config.lab2d_settings.simulation.map = _LUA_RANDOMIZATION_MAP
+    lab2d_settings = copy.deepcopy(_TEST_SETTINGS)
+    lab2d_settings.simulation.map = _LUA_RANDOMIZATION_MAP
 
-    env1 = self.enter_context(builder.builder(**config))
-    env2 = self.enter_context(builder.builder(**config))
+    env1 = self.enter_context(builder.builder(lab2d_settings))
+    env2 = self.enter_context(builder.builder(lab2d_settings))
     for episode in range(5):
       obs1 = env1.reset().observation['WORLD.RGB'][_LUA_RANDOMIZED_LINE]
       obs2 = env2.reset().observation['WORLD.RGB'][_LUA_RANDOMIZED_LINE]
