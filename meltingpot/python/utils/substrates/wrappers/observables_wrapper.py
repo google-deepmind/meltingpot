b2c49497eb9c26bf91209e24bb168d03159e9edf
Edgar Duéñez-Guzmán <duenez@google.com>
2023-01-27 10:29:11 -0800

    An Observables wrapper for Lab2D environments.
    PiperOrigin-RevId: 505151623
    Change-Id: Id3a892cfa68d4ffb623834936d7c33a5d6fee30e

---
+++ meltingpot/python/utils/substrates/wrappers/observables_wrapper.py
@@ -0,0 +1,74 @@
+# Copyright 2020 DeepMind Technologies Limited.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Wrapper that exposes Lab2d timesteps, actions, and events as observables."""
+
+from typing import Mapping, Union
+
+import dm_env
+import numpy as np
+from rx import subject
+
+import dmlab2d
+from meltingpot.python.utils.substrates.wrappers import observables
+
+
+Action = Union[int, float, np.ndarray]
+
+
+class ObservablesWrapper(observables.ObservableLab2dWrapper):
+  """Wrapper exposes timesteps, actions, and events as observables."""
+
+  def __init__(self, env: dmlab2d.Environment):
+    """Initializes the object.
+
+    Args:
+      env: The environment to wrap.
+    """
+    super().__init__(env)
+    self._action_subject = subject.Subject()
+    self._timestep_subject = subject.Subject()
+    self._events_subject = subject.Subject()
+    self._observables = observables.Lab2dObservables(
+        action=self._action_subject,
+        events=self._events_subject,
+        timestep=self._timestep_subject,
+    )
+
+  def reset(self) -> dm_env.TimeStep:
+    """See base class."""
+    timestep = super().reset()
+    self._timestep_subject.on_next(timestep)
+    for event in super().events():
+      self._events_subject.on_next(event)
+    return timestep
+
+  def step(self, action: Mapping[str, Action]) -> dm_env.TimeStep:
+    """See base class."""
+    self._action_subject.on_next(action)
+    timestep = super().step(action)
+    self._timestep_subject.on_next(timestep)
+    for event in super().events():
+      self._events_subject.on_next(event)
+    return timestep
+
+  def close(self) -> None:
+    """See base class."""
+    super().close()
+    self._action_subject.on_completed()
+    self._timestep_subject.on_completed()
+    self._events_subject.on_completed()
+
+  def observables(self) -> observables.Lab2dObservables:
+    """See base class."""
+    return self._observables
---
+++ meltingpot/python/utils/substrates/wrappers/observables_wrapper_test.py
@@ -0,0 +1,67 @@
+# Copyright 2020 DeepMind Technologies Limited.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for observables_wrapper."""
+
+import dataclasses
+from unittest import mock
+
+from absl.testing import absltest
+from absl.testing import parameterized
+
+import dmlab2d
+from meltingpot.python.utils.substrates.wrappers import observables_wrapper
+
+
+class ObservablesWrapperTest(parameterized.TestCase):
+
+  def test_observables(self):
+    base = mock.create_autospec(
+        dmlab2d.Environment, instance=True, spec_set=True)
+    with observables_wrapper.ObservablesWrapper(base) as env:
+      received = []
+      observables = env.observables()
+      for field in dataclasses.fields(observables):
+        getattr(observables, field.name).subscribe(
+            on_next=received.append,
+            on_error=lambda e: received.append(type(e)),
+            on_completed=lambda: received.append('DONE'),
+        )
+
+      base.reset.return_value = mock.sentinel.timestep_0
+      base.events.return_value = [mock.sentinel.events_0]
+      env.reset()
+      base.step.return_value = mock.sentinel.timestep_1
+      base.events.return_value = [mock.sentinel.events_1]
+      env.step(mock.sentinel.action_1)
+      base.step.return_value = mock.sentinel.timestep_2
+      base.events.return_value = [mock.sentinel.events_2]
+      env.step(mock.sentinel.action_2)
+
+    self.assertSequenceEqual(received, [
+        mock.sentinel.timestep_0,
+        mock.sentinel.events_0,
+        mock.sentinel.action_1,
+        mock.sentinel.timestep_1,
+        mock.sentinel.events_1,
+        mock.sentinel.action_2,
+        mock.sentinel.timestep_2,
+        mock.sentinel.events_2,
+        'DONE',
+        'DONE',
+        'DONE',
+    ])
+
+
+if __name__ == '__main__':
+  absltest.main()
