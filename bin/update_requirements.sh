c21315289e0d4120fe52194561774d11fcea05da
John Agapiou <jagapiou@google.com>
2025-12-01 04:17:21 -0800

    Include all extras in requirements.txt
    PiperOrigin-RevId: 838708840
    Change-Id: Iaac9a30c7291c0b1acdc8aa530cf5090e993341c

--- bin/update_requirements.sh
+++ bin/update_requirements.sh
@@ -18,5 +18,4 @@
 set -euxo pipefail
 cd "$(dirname "$0")/.."
 
-pip-compile --generate-hashes --reuse-hashes --strip-extras  --allow-unsafe \
-  --upgrade --extra dev setup.py
+pip-compile --upgrade
