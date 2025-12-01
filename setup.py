6d00b7b83617a65ad219a1ba85c19929cce1e441
John Agapiou <jagapiou@google.com>
2025-12-01 04:20:39 -0800

    Update setup.py to add 3.14 support classifiers
    PiperOrigin-RevId: 838709749
    Change-Id: Ic8c8343641e511d87a64b881a0cfcb00c474efdd

--- setup.py
+++ setup.py
@@ -126,6 +126,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
     cmdclass={'build_py': BuildPy},
