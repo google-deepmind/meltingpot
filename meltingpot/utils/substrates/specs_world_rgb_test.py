89346714395f1bba1d5d7387d3afb96094d529fd
Sylvester Kaczmarek <16242628+sylvesterkaczmarek@users.noreply.github.com>
2026-08-20 15:21:33 +0100

    Add regression test for WORLD.RGB map spaces

---
+++ meltingpot/utils/substrates/specs_world_rgb_test.py
@@ -0,0 +1,31 @@
+# Copyright 2026 DeepMind Technologies Limited.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Regression tests for WORLD.RGB spec sizing."""
+
+from absl.testing import absltest
+from meltingpot.utils.substrates import specs
+
+
+class WorldRgbTest(absltest.TestCase):
+
+  def test_preserves_spaces_on_boundary_rows(self):
+    ascii_map = '\n  A\n BB\n'
+
+    actual = specs.world_rgb(ascii_map, sprite_size=8)
+
+    self.assertEqual(actual.shape, (16, 24, 3))
+
+
+if __name__ == '__main__':
+  absltest.main()
